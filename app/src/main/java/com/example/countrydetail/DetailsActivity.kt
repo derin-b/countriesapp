170e11bd92a8341d21ba65777620dd3305ea5437
derin_ <derinola.bankole@gmail.com>
2022-11-10 16:14:56 +0100

    added the UI screens

--- app/src/main/java/com/example/countrydetail/DetailsActivity.kt
+++ app/src/main/java/com/example/countrydetail/DetailsActivity.kt
@@ -2,13 +2,13 @@ package com.example.countrydetail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.countrydetail.databinding.ActivityMainBinding
+import com.example.countrydetail.databinding.ActivityDetailsBinding
 
-class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+class DetailsActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
